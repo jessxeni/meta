c9ceea975bf378987e9e1c2ce7dcef46e02f02e0
jessxeni <100079376+jessxeni@users.noreply.github.com>
2022-02-20 15:44:08 +0100

    Add files via upload

---
+++ meta.js
@@ -0,0 +1,29 @@
+function meta() {
+	var text_m = ' hat ca 2,8 Milliarden Nutzer täglich. Neben Facebook der ersten App des 2021 umbennanten Konzerns Meta gehören zu ihnen auch die später aufgekauften Apps Instagram, WhatsApp und Snapchat. Meta hat die Vision eine "App-Familie" zu erstellen, in der jede App eine Funktion wahrnimmt. Alles zusammen nennen sie das "Metaversum".'
+	document.write(text_m)
+}
+
+function facebook() {
+	var text_fb = " ist ein soziales Netzwerk. Auf Facebook kann man Profile zur Darstellung zur privaten, geschäftlichen oder künstlerischen Präsenz, sowie Gruppen erstellen. Durch Freundschaften und Abonnements sind die Profile untereinander vernetzt."
+	document.write(text_fb)
+}
+
+function whatsapp() {
+	var text_wa = " ist ein Instant-Messaging-Dienst. Über WhatsApp können kostenlos Textnachrichten, Bild-, Video- und Ton-Dateien sowie Standortinformationen, Dokumente und Kontaktdaten zwischen zwei Personen oder in Gruppen ausgetauscht werden. Zudem ist internetbasiertes Telefonieren mit bis zu 8 Personen möglich."
+	document.write(text_wa)
+}
+
+function instagram() {
+	var text_ig = " ist ein soziales Netzwerk. Auf Instagram kann man Fotos und Video aufnehmen, bearbeiten und veröffentlichen. Durch kommentieren, liken und chatten kann man miteinander interagieren und sich vernetzten."
+	document.write(text_ig)
+}
+
+function messenger() {
+	var text_ms = " ist ein Instant-Messaging-Dienst. Mit Messenger kann man Nachrichten, Fotos, Videos, Sticker, Audio und Dateien teilen. Zudem ist Sprach- und Videotelefonie möglich und man kann auf geteilte Nachrichten reagieren und Spiele spielen."
+	document.write(text_ms)
+}
+
+function zuckerberg() {
+	var text_zb = " ist Gründer von Facebook und Meta, bei dem er zusätzlich Vorstandsvositzender ist und 28% Anteil besitzt. Laut der Forbes-Liste 2020 ist er mit 78 Milliarden US-Dollar der 7. reichste Mensch der Welt."
+	document.write(text_zb)
+}
